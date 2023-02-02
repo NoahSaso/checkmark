ff3d0c3dff2f4cb467de92fa2eaba5b1cfc7e9c0
Noah Saso <noahsaso@gmail.com>
2023-02-02 14:44:58 -0800

    Log responses to console.

--- pending-checkmarks-cf-worker/src/utils/response.ts
+++ pending-checkmarks-cf-worker/src/utils/response.ts
@@ -1,7 +1,9 @@
-export const respond = (status: number, response: Record<string, unknown>) =>
-  new Response(JSON.stringify(response), {
+export const respond = (status: number, response: Record<string, unknown>) => {
+  console.log('response', status, response)
+  return new Response(JSON.stringify(response), {
     status,
   })
+}
 
 export const respondError = (status: number, error: string) =>
   respond(status, { error })
